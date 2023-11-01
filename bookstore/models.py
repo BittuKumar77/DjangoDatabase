3b8032731c1c7b53da472ef03cf5fb6f3e63a74e
Bittu <bittusoni302@gmail.com>
2023-11-01 16:24:56 +0530

    oneNov

--- bookstore/models.py
+++ bookstore/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.db.models.base import Model
 
+from django.core.validators import MaxValueValidator,MinValueValidator
+
 
 # Create your models here.            
 
@@ -10,6 +12,7 @@ class bookdata(models.Model):
     author = models.CharField(max_length=50)
     price = models.IntegerField()
     best_seller = models.BooleanField(default=False)    
+    ratings = models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(10)],null=True)
 
     def __str__(self):
         return f"{self.bname} {self.author} {self.price} {self.best_seller}"                                
